7de6112b16dc6c1519d8cfd0d212e2e5959e4466
Brian <brian.stith0331@gmail.com>
2024-04-26 15:21:18 -0400

    commented working menu for home screen

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -10,12 +10,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Product> productsList = new ArrayList<>();
 
-        Product product = new Product(null, null, 0.0, null); // Providing default values for now
+        Product product = new Product(null, null, 0.0, null);
         product.loadProducts("Product.txt", productsList);
 
-        // Now you can use the productsList ArrayList as needed
+
         for (Product p : productsList) {
-            System.out.println(p); // Print each product
+            System.out.println(p);
         }
     }
 }
